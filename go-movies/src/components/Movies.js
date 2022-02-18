bf71bd943e9b08d354a755de463e0bfb1127f845
Daiki <dkub0001@student.monash.edu>
2022-02-18 17:47:45 +0900

    47 - 52

--- go-movies/src/components/Movies.js
+++ go-movies/src/components/Movies.js
@@ -1,33 +1,69 @@
 import React, {Component, Fragment} from 'react';
 import {Link} from 'react-router-dom';
 export default class Movies extends Component {
-    state = {movies: []};
+    state = {
+        movies: [],
+        isLoaded: false,
+        error: null,
+    };
 
     componentDidMount() { // this is to learn the lifecycle of react see lecture 31
         // executes after the component is rendered to the screen.
         // after the user clicks the movies from the App.js then it mounts the state with the populated information
-        this.setState({
-            movies: [
-                {id: 1, title: "The shawshank redemption", runtile: 142},
-                {id: 2, title: "The GodFather", runtile: 175},
-                {id: 3, title: "The Dark Knight", runtile: 153}
-            ]
-        })
+        // this.setState({
+        //     movies: [
+        //         {id: 1, title: "The shawshank redemption", runtile: 142},
+        //         {id: 2, title: "The GodFather", runtile: 175},
+        //         {id: 3, title: "The Dark Knight", runtile: 153}
+        //     ]
+        // })
+        fetch("http://localhost:4000/v1/movies")
+            // .then((response) => response.json())
+            .then((response) => {
+                console.log("Status Code is", response.status);
+                if (response.status != 200) {
+                    let err = Error;
+                    err.message = "Invalid Response Code: " + response.status;
+                    this.setState({error: err});
+                }
+                return response.json();
+            })
+            .then((json) => {
+                this.setState({
+                    movies: json.movies,
+                    isLoaded: true
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error: error 
+                    })
+                }
+                );
+            });
     }
 
     render() {
-        return (
-            <Fragment>
-            <h2>Choose a movie</h2>
-
-            <ul>
-                {this.state.movies.map( (m) => (
-                    <li key={m.id}>
-                        <Link to={`/movies/${m.id}`}>{m.title}</Link>
-                    </li>
-                ))}
-            </ul>
-            </Fragment>
-        );
+        const { movies, isLoaded, error } = this.state;
+        if (error) {
+            return <div> Error: {error.message} </div>
+        }
+        else if (!isLoaded) {
+            return <p>Loading ...</p>
+        } else {
+            return (
+                <Fragment>
+                <h2>Choose a movie</h2>
+    
+                <ul>
+                    {movies.map( (m) => (
+                        <li key={m.id}>
+                            <Link to={`/movies/${m.id}`}>{m.title}</Link>
+                        </li>
+                    ))}
+                </ul>
+                </Fragment>
+            );
+        }
     }
 }
--- go-movies/src/components/OneMovie.js
+++ go-movies/src/components/OneMovie.js
@@ -1,35 +1,88 @@
 import React, {Component, Fragment} from 'react';
 
 export default class OneMovie extends Component {
-    state = { movie: {} }
+    state = { movie: {} , isLoaded: false, error: null}
 
     componentDidMount() {
-        this.setState({
-            movie: {
-                id: this.props.match.params.id,
-                title: "Some Movie",
-                runtime: 150
+        // this.setState({
+        //     movie: {
+        //         id: this.props.match.params.id,
+        //         title: "Some Movie",
+        //         runtime: 150
+        //     }
+        // })
+        fetch("http://localhost:4000/v1/movie/" + this.props.match.params.id)
+        .then((response) => {
+            console.log("Status Code is", response.status);
+            if (response.status != 200) {
+                let err = Error;
+                err.message = "Invalid Response Code: " + response.status;
+                this.setState({error: err});
             }
+            return response.json();
         })
+        .then((json) => {
+            this.setState({
+                movie: json.movie,
+                isLoaded: true
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error: error 
+                })
+            }
+            );
+        });
     }
     render() {
-        return (
+        const {movie, isLoaded, error} = this.state;
+        
+        if (movie.genres) {
+            movie.genres = Object.values(movie.genres);
+        } else { // if genres do not exist
+            movie.genres = [];
+        }
+        if (error) {
+            return <div> Error: {error.message} </div>
+        }
+        else if (!isLoaded) {
+            return <p>Loading ...</p>
+        } else {
+            return (
             <Fragment>
-                <h2> Movie: {this.state.movie.title}</h2>
+                <h2> Movie: {movie.title} ({movie.year})</h2>
+                <div className='float-start'>
+                    <small>Rating: {movie.mpaa_rating}</small>
+                </div>
+                <div className='float-end'>
+                        {movie.genres.map((m, index) => (
+                            <span className='badge bg-secondary me-1' key={index}>
+                                {m}
+                            </span>
+                        ))}
+                </div>
+                <div className="clearfix"></div>
+                <hr />
                 <table className="table table-compact table-striped">
                     <thead></thead>
                     <tbody>
                         <tr>
                             <td><strong>Title:</strong></td>
-                            <td>{this.state.movie.title}</td>
+                            <td>{movie.title}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Description:</strong></td>
+                            <td>{movie.description}</td>
                         </tr>
                         <tr>
                             <td><strong>Run time:</strong></td>
-                            <td>{this.state.movie.runtime} minutes</td>
+                            <td>{movie.runtime} minutes</td>
                         </tr>
                     </tbody>
                 </table>
             </Fragment>
-        );
+            );
+        }
     }
 }
